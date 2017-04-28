c3a48e7be7c3cae4594209fb6bd25ebc624dafc9
Stormi Hoebelheinrich <stormihoebe@gmail.com>
2017-04-28 09:59:05 -0700

    add question-tile and customize tile hbs

--- app/models/question.js
+++ app/models/question.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   author: DS.attr(),
-  question: DS.attr(),
+  title: DS.attr(),
   notes: DS.attr(),
   answers: DS.hasMany('answer', { async: true })
 });
