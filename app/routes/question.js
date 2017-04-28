dfbfa3f32875560925fa46a944782d718a09e860
Stormi Hoebelheinrich <stormihoebe@gmail.com>
2017-04-28 11:45:39 -0700

    add ability to create and save new answer

--- app/routes/question.js
+++ app/routes/question.js
@@ -16,9 +16,35 @@ actions: {
   },
     delete(question){
       if(confirm("Are you sure you want to delete this question?")){
+        var answer_deletions = question.get('answers').map(function(answer) {
           return answer.destroyRecord();
+        });
+        Ember.RSVP.all(answer_deletions).then(function(){
+          return question.destroyRecord();
+        });
         this.transitionTo('index');
       }
+    },
+    updateAnswer(answer, params) {
+      Object.keys(params).forEach(key=>{
+        if(params[key]!==undefined){
+          answer.set(key, params[key]);
+        }
+      });
+      answer.save();
+    },
+    saveAnswer(params) {
+      var newAnswer = this.store.createRecord('answer', params);
+      var question = params.question;
+      question.get('answers').addObject(newAnswer);
+      newAnswer.save().then(function() {
+        return question.save();
+      });
+      this.transitionTo('question', question);
+    },
+    destroyAnswer(answer) {
+      answer.destroyRecord();
+      this.transitionTo('question');
     }
   }
 });
